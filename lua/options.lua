3144bf6e1aaefeba24dd117ebf2be62737bbe94a
Bui Khanh Van <van.buikhanh@hotmail.com>
2025-07-23 11:14:26 +0200

    update for the 4 space algin

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,12 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Add the option for the configuration
+local o = vim.o
+
+-- set the indent to 4 space
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.expandtab = true
